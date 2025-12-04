82b94226cbb2347a38addb5572f266831cc196c7
23ema <emanuele.dajko8@gmail.com>
2025-12-04 14:53:53 +0100

    Refactor exercise-01.cpp to use consistent dimension definition and update mesh file name

--- lab-03/src/exercise-01.cpp
+++ lab-03/src/exercise-01.cpp
@@ -7,10 +7,12 @@ main(int /*argc*/, char * /*argv*/[])
   // 1. Define the Problem Parameters.
   // ---------------------------------
 
+  constexpr unsigned int dim = DiffusionReaction::dim;
+
   // The mesh file to use, respectively:
   // Lab Task 1.1 asks for "mesh/mesh-cube-10.msh".
   // Lab Task 1.2 asks for "mesh/mesh-cube-40.msh".
-  const std::string mesh_file_name = "../mesh/mesh-cube-20.msh";
+  const std::string mesh_file_name = "../mesh/mesh-cube-40.msh";
 
   // Polynomial degree "r".
   const unsigned int r = 1;
@@ -21,22 +23,18 @@ main(int /*argc*/, char * /*argv*/[])
   // Diffusion Coefficient mu(x):
   // mu = 100 if x < 1/2 or mu = 1 if x >= 1/2
   // Note: we use 1/2 = 0.5
-  const auto mu = [](const Point<DiffusionReaction::dim> &p) -> double {
+  const auto mu = [](const Point<dim> &p) -> double {
     if (p[0] < 0.5)
-      return 100;
+      return 100.0;
     else
-      return 1;
+      return 1.0;
   };
 
   // Reaction Coefficient (constant) sigma(x) = 1.
-  const auto sigma = [](const Point<DiffusionReaction::dim> & /*p*/) -> double {
-    return 1;
-  };
+  const auto sigma = [](const Point<dim> & /*p*/) -> double { return 1.0; };
 
   // Forcing coefficient (constant) f(x) = 1.
-  const auto f = [](const Point<DiffusionReaction::dim> & /*p*/) -> double {
-    return 1;
-  };
+  const auto f = [](const Point<dim> & /*p*/) -> double { return 1.0; };
 
   // 3. Initialize and Run the Solver.
   // ---------------------------------
