297c111014082fe9c34a6b2374a7d19585a5685d
23ema <emanuele.dajko8@gmail.com>
2025-12-05 11:55:07 +0100

    Update coefficients and parameters in exercise-01.cpp for Exercise 1.4 settings

--- lab-04/src/exercise-01.cpp
+++ lab-04/src/exercise-01.cpp
@@ -8,16 +8,21 @@ main(int argc, char *argv[])
 
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
-  const auto mu = [](const Point<dim> & /*p*/) { return 0.1; };
-  const auto f  = [](const Point<dim>  &/*p*/, const double  &/*t*/) {
+  // Define coefficients.
+  const auto mu = [](const Point<dim> & /*p*/) -> double { return 0.1; };
+  const auto f  = [](const Point<dim>  &/*p*/, const double  &/*t*/) -> double {
     return 0.0;
   };
 
+  // Initialize problem with Exercise 1.4 settings:
+  // T = 1.0
+  // Theta = 1.0 (Implicit Euler)
+  // Delta_t = 0.05
   Heat problem(/*mesh_filename = */ "../mesh/mesh-cube-10.msh",
                /* degree = */ 1,
                /* T = */ 1.0,
-               /* theta = */ 0.0,
-               /* delta_t = */ 0.0025,
+               /* theta = */ 1.0,
+               /* delta_t = */ 0.05,
                mu,
                f);
 
